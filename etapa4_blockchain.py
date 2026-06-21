65d64ab93c7a6875e5832053d4f77c21e1f0f789
Patricia Paola <maximianopatricia55@gmail.com>
2026-06-21 20:17:12 -0300

    Renomeando arquivos e removendo arquivos desnecessários

--- etapa4_blockchain.py
+++
@@ -1,8 +0,0 @@
-
-def registrarBlockchain():
-    with open("registro_blockchain.txt", "w") as arquivo:
-
-        arquivo.write("Registro de Analise\n")
-        arquivo.write("ransom_wallet - ALTO\n")
-
-    print("Registro realizado")
